31e885c610c177395485faf5778a213985ead39d
nbzAkash <akashgupta.nbz@gmail.com>
2023-01-05 09:26:44 +0530

    insert quries created

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,10 +3,30 @@ INSERT INTO `22-jrtp`.`country_master` (`countryId`, `countryName`) VALUES ('2',
 
 INSERT INTO `22-jrtp`.`state_master` (`stateId`, `stateName`, `countryId`) VALUES ('1', 'Florida', '2');
 INSERT INTO `22-jrtp`.`state_master` (`stateId`, `stateName`, `countryId`) VALUES ('2', 'Alaska', '2');
+INSERT INTO `22-jrtp`.`state_master` (`stateId`, `stateName`, `countryId`) VALUES ('3', 'Calofornia', '2');
+INSERT INTO `22-jrtp`.`state_master` (`stateId`, `stateName`, `countryId`) VALUES ('4', 'Kentuchy', '2');
+INSERT INTO `22-jrtp`.`state_master` (`stateId`, `stateName`, `countryId`) VALUES ('5', 'Michigan', '2');
 INSERT INTO `22-jrtp`.`state_master` (`stateId`, `stateName`, `countryId`) VALUES ('6', 'Uttar Pradesh', '1');
+INSERT INTO `22-jrtp`.`state_master` (`stateId`, `stateName`, `countryId`) VALUES ('7', 'Telangana', '1');
+INSERT INTO `22-jrtp`.`state_master` (`stateId`, `stateName`, `countryId`) VALUES ('8', 'Delhi', '1');
+INSERT INTO `22-jrtp`.`state_master` (`stateId`, `stateName`, `countryId`) VALUES ('9', 'Mumbai', '1');
+INSERT INTO `22-jrtp`.`state_master` (`stateId`, `stateName`, `countryId`) VALUES ('10', 'Rajasthan', '1');
+
 
 INSERT INTO `22-jrtp`.`cities_master` (`cityId`, `cityName`, `stateId`) VALUES ('1', 'Varanasi', '6');
+INSERT INTO `22-jrtp`.`cities_master` (`cityId`, `cityName`, `stateId`) VALUES ('2', 'Lucknow', '6');
+INSERT INTO `22-jrtp`.`cities_master` (`cityId`, `cityName`, `stateId`) VALUES ('3', 'Allahabad', '6');
+INSERT INTO `22-jrtp`.`cities_master` (`cityId`, `cityName`, `stateId`) VALUES ('4', 'Bhadohi', '6');
 INSERT INTO `22-jrtp`.`cities_master` (`cityId`, `cityName`, `stateId`) VALUES ('5', 'Hyderabad', '7');
-
+INSERT INTO `22-jrtp`.`cities_master` (`cityId`, `cityName`, `stateId`) VALUES ('6', 'Warangal', '7');
+INSERT INTO `22-jrtp`.`cities_master` (`cityId`, `cityName`, `stateId`) VALUES ('7', 'Nizamabad', '7');
+INSERT INTO `22-jrtp`.`cities_master` (`cityId`, `cityName`, `stateId`) VALUES ('8', 'New Delhi', '8');
+INSERT INTO `22-jrtp`.`cities_master` (`cityId`, `cityName`, `stateId`) VALUES ('9', 'Deoli', '8');
+INSERT INTO `22-jrtp`.`cities_master` (`cityId`, `cityName`, `stateId`) VALUES ('10', 'Mehrauli', '8');
+INSERT INTO `22-jrtp`.`cities_master` (`cityId`, `cityName`, `stateId`) VALUES ('11', 'Saket', '8');
+INSERT INTO `22-jrtp`.`cities_master` (`cityId`, `cityName`, `stateId`) VALUES ('12', 'Andheri', '9');
+INSERT INTO `22-jrtp`.`cities_master` (`cityId`, `cityName`, `stateId`) VALUES ('13', 'Bandra', '9');
+INSERT INTO `22-jrtp`.`cities_master` (`cityId`, `cityName`, `stateId`) VALUES ('14', 'Borivali', '9');
+INSERT INTO `22-jrtp`.`cities_master` (`cityId`, `cityName`, `stateId`) VALUES ('15', 'Juhu', '9');
 
 
